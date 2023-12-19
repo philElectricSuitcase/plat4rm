16d45c8186f63f97c6fc9a3ffe0d73259e1695d2
philElectricSuitcase <=>
2023-12-19 12:55:18 +0000

    initial

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import plat4rm from './assets/plat4rm.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+        
+          <img src={plat4rm} className='center' />
+       </div>
+    </>
+  )
+}
+
+export default App
